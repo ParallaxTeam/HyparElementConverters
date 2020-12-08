73902ca7cc706af4843130a91bcce5936159125b
john pierson <sixtysecondrevit@gmail.com>
2020-12-08 16:40:09 -0700

    CurtainWallConverter - Added sample RVT, implemented model curves for U Grids

--- src/CurtainWall/HyparRevitCurtainWallConverter/CurtainWallConverter.cs
+++ src/CurtainWall/HyparRevitCurtainWallConverter/CurtainWallConverter.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Elements;
 using ADSK = Autodesk.Revit.DB;
 using Elements.Conversion.Revit;
+using Elements.Conversion.Revit.Extensions;
+using Elements.Geometry;
 using Element = Elements.Element;
 
 namespace HyparRevitCurtainWallConverter
 {
     public class CurtainWallConverter : IRevitConverter
     {
+        public bool CanConvertToRevit => false;
+        public bool CanConvertFromRevit => true;
+
         public ADSK.FilteredElementCollector AddElementFilters(ADSK.FilteredElementCollector collector)
         {
             //pre-collect curtain walls by seeing if they have a curtain grid. also exclude in-place families.
@@ -25,12 +31,7 @@ namespace HyparRevitCurtainWallConverter
 
         public Element[] FromRevit(ADSK.Element revitElement, ADSK.Document document)
         {
-            throw new NotImplementedException();
-        }
-
-        public Element[] FromRevit(Element revitElement, ADSK.Document document)
-        {
-            throw new NotImplementedException();
+            return MakeHyparCurtainWallFromRevitCurtainWall(revitElement);
         }
 
         public ADSK.ElementId[] ToRevit(Element hyparElement, LoadContext context)
@@ -43,7 +44,34 @@ namespace HyparRevitCurtainWallConverter
             throw new NotImplementedException();
         }
 
-        public bool CanConvertToRevit { get; }
-        public bool CanConvertFromRevit { get; }
+        private static Element[] MakeHyparCurtainWallFromRevitCurtainWall(ADSK.Element revitElement)
+        {
+            var doc = revitElement.Document;
+            var curtainWall = revitElement as ADSK.Wall;
+
+            if (curtainWall.CurtainGrid == null)
+            {
+                throw new InvalidOperationException("This curtain wall does not have a grid. Curtain walls with no grid are not supported at this time.");
+            }
+
+            var uGridLines = curtainWall.CurtainGrid.GetUGridLineIds()
+                .Select(c => doc.GetElement(c) as ADSK.CurtainGridLine).ToArray();
+
+            var lines = uGridLines.Select(u =>
+                new Line(u.FullCurve.GetEndPoint(0).ToVector3(), u.FullCurve.GetEndPoint(1).ToVector3())).ToArray();
+
+            var modelCurves = new List<ModelCurve>(); 
+            foreach (var uGrid in uGridLines)
+            {
+                var fullCurve = uGrid.FullCurve;
+
+               Curve curve = new Line(fullCurve.GetEndPoint(0).ToVector3(), fullCurve.GetEndPoint(1).ToVector3());
+
+               modelCurves.Add(new ModelCurve(curve));
+            }
+
+            return modelCurves.ToArray();
+
+        }
     }
 }
