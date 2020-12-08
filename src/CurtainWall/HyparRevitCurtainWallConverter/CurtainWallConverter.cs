9a90396c915876dba1850ec6d78481a02a4df22e
john pierson <sixtysecondrevit@gmail.com>
2020-12-08 15:52:28 -0700

    Curtain Wall collector via filter

--- src/CurtainWall/HyparRevitCurtainWallConverter/CurtainWallConverter.cs
+++ src/CurtainWall/HyparRevitCurtainWallConverter/CurtainWallConverter.cs
@@ -13,8 +13,14 @@ namespace HyparRevitCurtainWallConverter
     {
         public ADSK.FilteredElementCollector AddElementFilters(ADSK.FilteredElementCollector collector)
         {
-            //collect walls that are instance based on not in-place families
-            return collector.OfCategory(ADSK.BuiltInCategory.OST_Walls).WhereElementIsNotElementType().WherePasses(new ADSK.ElementClassFilter(typeof(ADSK.Wall)));
+            //pre-collect curtain walls by seeing if they have a curtain grid. also exclude in-place families.
+            var curtainWallIds = collector.OfCategory(ADSK.BuiltInCategory.OST_Walls).WhereElementIsNotElementType().WherePasses(new ADSK.ElementClassFilter(typeof(ADSK.Wall))).Cast<ADSK.Wall>().Where(w => w.CurtainGrid != null).Select(w => w.Id).ToArray();
+
+            //generate a revit filter based on those ids
+            ADSK.ElementFilter curtainWallIdFilter = new ADSK.ElementIdSetFilter(curtainWallIds);
+
+            //collect the curtain walls using the filter
+            return collector.OfCategory(ADSK.BuiltInCategory.OST_Walls).WherePasses(curtainWallIdFilter);
         }
 
         public Element[] FromRevit(ADSK.Element revitElement, ADSK.Document document)
